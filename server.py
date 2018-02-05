438ec029ddec9ef0ad4f9730abf68c44fe13218c
vishay28 <vishay28@gmail.com>
2018-02-05 19:55:35 +0000

    Update server.py

--- server.py
+++ server.py
@@ -2,9 +2,12 @@
 import socket
 #Imports a package to get the current date and time for timestamping
 import datetime
+#Imports the time package
 import time
+#Imports the threading package for multithreading
 from threading import Thread
 
+#Setting the client message to blank
 clientMessage = ""
 
 #Creating a function to get the current date and time and formatting it
@@ -16,38 +19,74 @@ def getTime():
     #Returning the current date and time
     return currentTime
 
+#A function to send messages to the light client
+def lightClientSendMessage(message):
+    #Sending the message to the light client
+    lightClient.send(message.encode())
+
+#A function to send messages to the user client
+def userClientSendMessage(message):
+    #Sending the message to the light client
+    userClient.send(message.encode())
+
+#A function to constantly check if the light client is connected
 def sendLightActive():
+    #Creating a global variable to store whether the light error message has already been shown or not
     global lightMessageDisplayed
+    #Creating a main loop in which the server will send the light client a message to check whether it is active
     while True:
+        #Trying to send the light client a message
         try:
+            #Waiting for a second before sending the next message
             time.sleep(1)
-            sendMessage = "active"
-            lightClient.send(sendMessage.encode())
+            #Attemting to send a message to the light client
+            lightClientSendMessage("active")
+        #A name error will occur if the the light client hasn't been initialised yet
         except NameError:
+            #If this error occurs we just pass
             pass
-        except ConnectionAbortedError:
+        #A connection abort error or a connection reset error will occcur if the light client disconnects
+        except (ConnectionAbortedError, ConnectionResetError):
+            #Try to send the user a message letting it know that the light has been disconnected
             try:
+                #Checking whether the error message for the light has already been displayed
                 if lightMessageDisplayed == False:
-                    sendMessage = "lightError"
+                    #Printing to the server that the light has been disconnected
                     print(getTime() + "Light disconnected")
+                    #The message has now been displayed so we can change the variable to true
                     lightMessageDisplayed = True
-                    userClient.send(sendMessage.encode())
+                    #Attempting to send the user client a message informing it of the light being disconnected
+                    userClientSendMessage("lightError")
+                    #Resetting the message to nothing
                     sendMessage=""
-            except ConnectionResetError:
+            #The connection reset error could occur here if the client disconnects first and then the light disconnects
+            #The name error could occur here if the user client hasn't been initialised yet
+            except (ConnectionResetError, NameError):
+                #If this error occurs we just pass
                 pass
+            #If none of the above work then just pass
             pass
 
+#A function to listen for a connecting device
 def listenForDevice():
+    #Creating a global varialble to store what the device type is
     global deviceType
     #Getting the server to listen for a device to connect to it
     while True:
+        #Listening for a single device
         server.listen(1)
+        #Accepting the incomming connection and getting the client name and address
         client1, client1Address = server.accept()
+        #Listening for what the device ID is
         deviceType = client1.recv(1024)
+        #Decoding the message received
         deviceType = deviceType.decode()
+        #Passing the device ID, name and address to a function to determing which device it is
         determineDeviceType(deviceType, client1, client1Address)
 
+#A function to determine which device has connected to the server
 def determineDeviceType(clientID, client, clientAddress):
+    #Assigning a global variables to all the clients and the booleans that determine whether their error message has been displayed
     global deviceType
     global switchClient
     global lightClient
@@ -55,45 +94,71 @@ def determineDeviceType(clientID, client, clientAddress):
     global lightMessageDisplayed
     global userMessageDisplayed
     global switchMessageDisplayed
+    #Checking if the device ID is a switch
     if deviceType == "switch":
+        #If it is a switch then printing to the server that the switch has connected
         print(getTime() + "Connected to switch " + str(clientAddress))
+        #Assigning switchClient to the client that has just connected
         switchClient = client
+        #Setting the error message displayed to false as the device has successfully connected back again so the error hasn't been shown yet
         switchMessageDisplayed = False
+    #Checking if the device ID is a light
     elif deviceType == "light":
+        #If it is a light then printing to the server that the light has connected
         print(getTime() + "Connected to light " + str(clientAddress))
+        #Assigning lightClient to the client that has just connected
         lightClient = client
+        #Setting the error message displayed to false as the device has successfully connected back again so the error hasn't been shown yet
         lightMessageDisplayed = False
+        #Attempting to send the user client a message letting it know that the light has connected
         try:
-            sendMessage=("lightConnected")
-            userClient.send(sendMessage.encode())
+            userClientSendMessage("lightConnected")
+        #The name error may occur as the user client may not be initialised yet
+        #the connection reset error could occur as the user may have also disconnected
         except (NameError, ConnectionResetError):
+            #If the errors above occur then just pass
             pass
+    #Checking if the device ID is a user
     elif deviceType == "user":
+        #If it is a user then printing to the server that the user has connected
         print(getTime() + "Connected to user " + str(clientAddress))
+        #Assigning userClient to the client that has just connected
         userClient = client
+        #Setting the error message displayed to false as the device has successfully connected back again so the error hasn't been shown yet
         userMessageDisplayed = False
 
+#A function to control the outputs based upon the switch status
 def switchControl():
+    #Creating the global variable to see what the clients are sending
     global clientMessage
+    #Creating a global variable to check whether the error message for the switch has been displayed
     global switchMessageDisplayed
 
+    #Creating a main loop in which the server can listen to the switch and respond to it
     while True:
         try:
-            time.sleep(0.5)
+            #This time delay has been added so that if the switch disconnects and then recoonects it doesn't display switch disconnected after it reconnecting
+            time.sleep(0.1)
+
+            #Listening for the switch and recieving any message that is being sent to the server
             clientMessage = switchClient.recv(1024)
+            #Decoding the message that has been sent
             clientMessage = clientMessage.decode()
 
+            #Determining what the switch message was and responding accordingly
+            #If the switch sent the message to switch on
             if clientMessage == "switchOn":
+                #Print that that the switch and the light have both been turned on
                 print(getTime() + "Switch turned on")
                 print(getTime() + "Light turned on")
-                sendMessage = "turnOn"
-                lightClient.send(sendMessage.encode())
+                #Send a message to the light to switch on
+                lightClientSendMessage("turnOn")
+                #Resetting the message received
                 clientMessage = ""
             elif clientMessage == "switchOff":
                 print(getTime() + "Switch turned off")
                 print(getTime() + "Light turned off")
-                sendMessage = "turnOff"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage("turnOff")
                 clientMessage = ""
         except NameError:
             pass
@@ -108,52 +173,43 @@ def userControl():
     global userMessageDisplayed
     while True:
         try:
+            time.sleep(0.1)
             clientMessage = ""
             sendMessage = ""
             clientMessage = userClient.recv(1024)
             clientMessage = clientMessage.decode()
 
             if clientMessage == "switchOn":
-                sendMessage = "turnOn"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage("turnOn")
                 print(getTime() + "Light turned on")
             elif clientMessage == "switchOff":
-                sendMessage = "turnOff"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage("turnOff")
                 print(getTime() + "Light turned off")
             elif clientMessage == "red":
-                sendMessage = "red"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to red")
             elif clientMessage == "blue":
-                sendMessage = "blue"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to blue")
             elif clientMessage == "green":
-                sendMessage = "green"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to green")
             elif clientMessage == "purple":
-                sendMessage = "purple"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to purple")
             elif clientMessage == "yellow":
-                sendMessage = "yellow"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to yellow")
             elif clientMessage == "cyan":
-                sendMessage = "cyan"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to cyan")
             elif clientMessage == "white":
-                sendMessage = "white"
-                lightClient.send(sendMessage.encode())
+                lightClientSendMessage(clientMessage)
                 print(getTime() + "Light turned to white")
         except NameError:
             pass
         except ConnectionAbortedError:
-            sendMessage = "lightError"
-            userClient.send(sendMessage.encode())
+            userClientSendMessage("lightError")
             print(getTime() + "Light disconnected")
             sendMessage = ""
         except ConnectionResetError:
