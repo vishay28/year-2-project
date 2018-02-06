f557c3bf7b87018d057c8f91053852dd0dc13700
vishay28 <vishay28@gmail.com>
2018-02-06 17:13:57 +0000

    Update switch.py

--- switch.py
+++ switch.py
@@ -2,7 +2,9 @@
 import socket
 #Imports a package to get the current date and time for timestamping
 import datetime
+#Imports the multithreading package
 from threading import Thread
+#Imports the time package
 import time
 
 #Creating a function to get the current date and time and formatting it
@@ -14,13 +16,15 @@ def getTime():
     #Returning the current date and time
     return currentTime
 
+#A function to send the server a message
 def serverMessageSend(message):
+    #Sending the message to the server
     server.send(message.encode())
 
 
-
+#A main method to run the program
 if __name__ == "__main__":
-    #Asking the user to input the ip of the server it is trying to connect to. (USE "localhost" if you are running both programs on the same computer)
+    #Asking the user to input the ip of the server it is trying to connect to. (USE "localhost" if you are running the programs on the same computer)
     print("Input the server ip")
     #Getting the user input for the ip
     ip = input()
@@ -39,13 +43,17 @@ if __name__ == "__main__":
     #Once successfully connected it prints a log of the date and time and the ip it has connected to
     print(getTime() + "Connected to " + ip)
 
+    #Setting the client ID and sending it to the server
     clientID = "switch"
-    server.send(clientID.encode())
+    serverMessageSend(clientID)
 
+    #Creating a main loop where the switch can provide inputs
     while True:
+        #Resetting the user input
         userInput = ""
-        message = ""
+        #Getting the user input
         userInput = input()
+        #Sending the server a message depending on the switch state
         if userInput == "on":
             serverMessageSend("switchOn")
         elif userInput == "off":
