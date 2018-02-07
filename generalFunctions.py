3f0d3c1e7dc5a05f511aff6b13d5513e0ebe6fdd
vishay28 <vishay28@gmail.com>
2018-02-07 13:33:27 +0000

    Update generalFunctions.py

--- generalFunctions.py
+++ generalFunctions.py
@@ -12,4 +12,21 @@ def getTime():
     #Returning the current date and time
     return currentTime
 
+def connectToServer():
+    #Asking the user to input the ip of the server it is trying to connect to. (USE "localhost" if you are running the programs on the same computer)
+    print("Input the server ip")
+    #Getting the user input for the ip
+    ip = input()
+    #Asking the user to input the port of the server it is trying to connect to
+    print("Input the port")
+    #Getting the user input for the port and then coverting it to an integer
+    port = int(input())
+    #Creatign a socket for the client to connect to the server
+    server = socket.socket()
+    #Trying to connect to the server using the ip and port specified by the user
+    server.connect((ip,port))
+    #Once successfully connected it prints a log of the date and time and the ip it has connected to
+    print(getTime() + "Connected to " + ip)
+    return server
+
 colourInputs = {"turnOff":[0,0,0],"red":[1,0,0], "blue":[0,1,0], "green":[0,0,1], "purple":[1,1,0], "yellow":[1,0,1], "cyan":[0,1,1], "white":[1,1,1]}
