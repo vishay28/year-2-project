74096c0bcfdba28cd0cb84173efbc24a5f5a5426
vishay28 <vishay28@gmail.com>
2018-02-26 10:09:40 +0000

    Update user.py

--- user.py
+++ user.py
@@ -1,60 +1,75 @@
 from generalFunctions import *
+import pymongo
 
 
-#A function that will constantly listen for messages being sent by the server
-def serverListen():
-    global lightConnected
-    while True:
-        #Listening for a message from the server
-        serverMessage = server.recv(1024)
-        #Decoding the message from the server
-        serverMessage = serverMessage.decode()
-        #Readting to different messages received from the server
-        if serverMessage == "lightError":
-            print(getTime() + "Light disconnected")
-            #Setting the value of light connected to false
-            lightConnected = False
-            serverMessage = ""
-        elif serverMessage == "lightConnected":
-            print(getTime() + "Light connected")
-            #Setting the value of light connected to true
-            lightConnected = True
-            serverMessage = ""
 #A function to send the server a message
 def serverMessageSend(message):
     server.send(message.encode())
 
+def lightCheck():
+    newMessage = ""
+    message = ""
+
+
+    #Creating a main loop in whcih the user can send messages to the server
+    while True:
+        lightsDict = devicesDB.lights.find({})
+        try:
+            for document in lightsDict:
+                time.sleep(1)
+                RGBValues = devicesDB.lights.find_one({"id": document["id"]})
+                r = RGBValues["r"]
+                g = RGBValues["g"]
+                b = RGBValues["b"]
+                if ((r==0) and (g==0) and (b==0)):
+                    userInput = "switchOff"
+                elif ((r==0) and (g==0) and (b==1)):
+                    userInput = "blue"
+                elif ((r==0) and (g==1) and (b==0)):
+                    userInput = "green"
+                elif ((r==0) and (g==1) and (b==1)):
+                    userInput = "cyan"
+                elif ((r==1) and (g==0) and (b==0)):
+                    userInput = "red"
+                elif ((r==1) and (g==0) and (b==1)):
+                    userInput = "purple"
+                elif ((r==1) and (g==1) and (b==0)):
+                    userInput = "yellow"
+                elif ((r==1) and (g==1) and (b==1)):
+                    userInput = "white"
+                serverMessageSend(document["id"])
+                serverMessageSend(userInput)
+        except TypeError:
+            pass
+
+def plugCheck():
+    while True:
+        plugsDict = devicesDB.plugs.find({})
+        try:
+            for document in plugsDict:
+                time.sleep(1)
+                plugsData = devicesDB.plugs.find_one({"id": document["id"]})
+                state = plugsData["state"]
+                if state:
+                    userInput = "turnOn"
+                elif not state:
+                    userInput = "turnOff"
+                serverMessageSend(document["id"])
+                serverMessageSend(userInput)
+        except TypeError:
+            pass
+
 #Creating a main method in which to run the program
 if __name__ == "__main__":
     server = connectToServer()
-
-    #Creating a thread to constantly listen to the
-    serverListenThread = Thread(target=serverListen)
-    #Starting the thread
-    serverListenThread.start()
+    dbClient = pymongo.MongoClient("localhost", 27017)
+    devicesDB = dbClient.devices
 
     #Setting the client ID and sending it to the server
     clientID = "user"
     serverMessageSend(clientID)
 
-    #Defining a global variable to determine whether the light is connected or not
-    global lightConnected
-    #Setting the default value to true
-    lightConnected = True
-
-    #Creating a main loop in whcih the user can send messages to the server
-    while True:
-        #Resetting the user input
-        userInput = ""
-        #Getting the user input
-        userInput = input()
-        #Depending on what the user types different messages will be sent to the server
-        #If the light is disconnected then don't allow the user to send a message to the server
-        if lightConnected == False:
-            print("Light disconnected")
-        elif userInput == "on":
-            serverMessageSend("switchOn")
-        elif userInput == "off":
-            serverMessageSend("switchOff")
-        elif userInput in colourInputs:
-            serverMessageSend(userInput)
+    lightThread = Thread(target=lightCheck)
+    plugThread = Thread(target=plugCheck)
+    lightThread.start()
+    plugThread.start()
