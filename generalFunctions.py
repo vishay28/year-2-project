aec92f8e52a787ecd2ce4eda18bcb394c85b6a7b
vishay28 <vishay28@gmail.com>
2018-04-11 11:10:53 +0100

    Update generalFunctions.py

--- generalFunctions.py
+++ generalFunctions.py
@@ -1,6 +1,11 @@
+#This program is impoted into most of the other programs as the functions defined in this class are common functions required by many programs
+#Importing threading to allow the program to run multiple threads
 from threading import Thread
+#Importing the datetime function to allow the program to interact with the current date and time
 import datetime
+#Importing the time function to allow the program to wait for a specific amount of time
 import time
+#Importing the socket function to allow the program to interact with TCP sockets
 import socket
 
 #Creating a function to get the current date and time and formatting it
@@ -12,10 +17,11 @@ def getTime():
     #Returning the current date and time
     return currentTime
 
+#A function to connect to the server
 def connectToServer():
-    #Asking the user to input the ip of the server it is trying to connect to. (USE "localhost" if you are running the programs on the same computer)
+    #Setting the ip address of the server to the fixed ip that the server was set up with
     ip = "192.168.0.10"
-    #Getting the user input for the port and then coverting it to an integer
+    #Setting the port number
     port = 5005
     #Creatign a socket for the client to connect to the server
     server = socket.socket()
@@ -23,6 +29,8 @@ def connectToServer():
     server.connect((ip,port))
     #Once successfully connected it prints a log of the date and time and the ip it has connected to
     print(getTime() + "Connected to " + ip)
+    #Returning the server information
     return server
 
+#This array defines the GPIO outputs for different colours and states for the light
 colourInputs = {"turnOff":[0,0,0],"red":[1,0,0], "blue":[0,1,0], "green":[0,0,1], "purple":[1,1,0], "yellow":[1,0,1], "cyan":[0,1,1], "white":[1,1,1]}
